985142a47a15f9125a432a593f6adc889536548a
Андрей Милютин <andrew.milyutin@gmail.com>
2023-05-01 12:15:44 +0300

    started template

---
+++ src/main/java/com/example/todoJavaBackend/TodoJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoJavaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todoJavaBackend/TodoJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoJavaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
